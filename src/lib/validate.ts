bd6024d18ce0f7c7bc3bd9d9fe4098d61d0660ed
Chris Carrington <chris.carrington@glew.io>
2023-03-21 02:02:06 -0700

    update import & comment

--- src/lib/validate.ts
+++ src/lib/validate.ts
@@ -1,5 +1,5 @@
 import { CHALLENGE_URL, ERROR_MESSAGE } from './variables'
-import type { TurnstileTokenValidateResponse } from '$lib/index.d'
+import type { TurnstileTokenValidateResponse } from '$lib'
 
 
 export default async (turnstileResponse: FormDataEntryValue, secret: string) => {
@@ -11,7 +11,7 @@ export default async (turnstileResponse: FormDataEntryValue, secret: string) => 
 
   const validationResponse: TurnstileTokenValidateResponse = await response.json()
 
-  // The timeout-or-duplicate error code can happen @ UpdateUser.svelte where the file changes but the other form fields do not change
+  // The timeout-or-duplicate error code can happen @ form where the file changes but the other form fields do not change
   // This is not a scary error so lets just bypass it, all errors may be found here
   // https://developers.cloudflare.com/turnstile/get-started/server-side-validation/#error-codes
   if (!validationResponse.success && validationResponse['error-codes'].length === 1 && validationResponse['error-codes'][0] === 'timeout-or-duplicate') {
