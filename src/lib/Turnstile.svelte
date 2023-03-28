09e451a1f11d678f2ebc5fb3182d37204cfe3b87
Chris Carrington <chris.carrington@glew.io>
2023-03-27 17:30:46 -0700

    Call window.tunstile after it's been defined

--- src/lib/Turnstile.svelte
+++ src/lib/Turnstile.svelte
@@ -4,31 +4,48 @@
   export let sitekey: string
 
   let div: HTMLDivElement
+  const maxAttemptCount = 9
+  let readyAttemptCounter = 0
   const dispatch = createEventDispatcher()
 
   onMount(() => {
     let id: string
+    let timer: number
 
-    window.turnstile.ready(() => {
-      id = window.turnstile.render(div, {
-        sitekey,
-        callback () {
-          dispatch('state', { status: 'success', message: 'All good!' })
-        },
-        'error-callback' () {
-          dispatch('state', { status: 'error', message: 'Network error' })
-        },
-        'expired-callback' () {
-          dispatch('state', { status: 'expired', message: 'Token expired' })
-        },
-        'timeout-callback' () {
-          dispatch('state', { status: 'timeout', message: 'Challenge expired' })
-        },
+    function onReady () {
+      window.turnstile.ready(() => {
+        id = window.turnstile.render(div, {
+          sitekey,
+          callback () {
+            dispatch('state', { status: 'success', message: 'All good!' })
+          },
+          'error-callback' () {
+            dispatch('state', { status: 'error', message: 'Network error' })
+          },
+          'expired-callback' () {
+            dispatch('state', { status: 'expired', message: 'Token expired' })
+          },
+          'timeout-callback' () {
+            dispatch('state', { status: 'timeout', message: 'Challenge expired' })
+          },
+        })
       })
-    })
+    }
+
+    function attempt () {
+      if (window.turnstile) {
+        onReady()
+      } else if (readyAttemptCounter < maxAttemptCount) {
+        readyAttemptCounter++
+        timer = setTimeout(attempt, 81)
+      }
+    }
+
+    attempt()
 
     return () => { // on unmount
       if (id) window.turnstile.remove(id)
+      if (timer) window.clearTimeout(timer)
     }
   })
 </script>
